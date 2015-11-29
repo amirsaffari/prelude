9bb8e065725e9eb0f8e4bb46bfc54afa2d4d0bce
Amir <amir@ymer.org>
2015-11-29 11:29:36 +0000

    added venv

--- personal/amir-python.el
+++ personal/amir-python.el
@@ -1,2 +1,9 @@
-;;====== Pyrex/Cython
+;; Cython
 (load "cython-mode")
+
+;; Virtualenvwrapper
+(require 'virtualenvwrapper)
+(venv-initialize-interactive-shells) ;; if you want interactive shell support
+(venv-initialize-eshell) ;; if you want eshell support
+(setq venv-location '("/home/amir/projects/import.io/marvin/comp_name_env/"))
+;;
