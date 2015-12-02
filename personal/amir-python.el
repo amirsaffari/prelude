8f2648b681d5095902a943d950eb9462153e64c8
Amir <amir@ymer.org>
2015-12-02 19:27:40 +0000

    ipython clean up

--- personal/amir-python.el
+++ personal/amir-python.el
@@ -1,6 +1,22 @@
 ;; Cython
 (load "cython-mode")
 
+;; IPython
+;; (setq
+;;  python-shell-interpreter "ipython"
+;;  python-shell-interpreter-args ""
+;;  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+;;  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+;;  python-shell-completion-setup-code
+;;  "from IPython.core.completerlib import module_completion"
+;;  python-shell-completion-module-string-code
+;;  "';'.join(module_completion('''%s'''))\n"
+;;  python-shell-completion-string-code
+;;  "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+(setq python-shell-interpreter "ipython")
+(setq python-shell-interpreter-args "--pylab")
+
 ;; Virtualenvwrapper
 (require 'virtualenvwrapper)
 (venv-initialize-interactive-shells) ;; if you want interactive shell support
